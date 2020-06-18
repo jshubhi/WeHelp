fb7d21888ba8b662753a0265783279d39b065d33
SuryaKanduri <63741874+SuryaKanduri@users.noreply.github.com>
2020-06-19 04:38:12 +0530

    7777777

--- app/src/main/java/com/example/wehelp/contactus.java
+++ app/src/main/java/com/example/wehelp/contactus.java
@@ -1,5 +1,6 @@
 package com.example.wehelp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -7,11 +8,51 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class contactus extends Fragment {
+    EditText csName,csContact,csSubject,csMessage;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.contactus,container,false);
+        final View inflate = inflater.inflate(R.layout.contactus, container, false);
+
+         csName = (EditText) inflate.findViewById(R.id.csName);
+         csContact = (EditText) inflate.findViewById(R.id.csContact);
+         csSubject = (EditText) inflate.findViewById(R.id.csSubject);
+         csMessage = (EditText) inflate.findViewById(R.id.csMessage);
+        Button csSubmit=(Button)inflate.findViewById(R.id.csSubmit);
+        csSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mailing();
+
+            }
+        });
+
+
+        return inflate;
     }
+    public void mailing()
+    {
+
+        String subject= csSubject.getText().toString();
+        String name= csName.getText().toString();
+        String contact= csContact.getText().toString();
+        String message= csMessage.getText().toString();
+        String All="NAME:"+name+"\nCONTACT:"+contact+"\n\n"+message;
+        Intent intent= new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL,new String[]{"fighterhanuman@gmail.com"});
+        intent.putExtra(Intent.EXTRA_SUBJECT,subject);
+        intent.putExtra(Intent.EXTRA_TEXT,All);
+
+
+
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent,"Choose an Email Client"));
+    }
+
 }
